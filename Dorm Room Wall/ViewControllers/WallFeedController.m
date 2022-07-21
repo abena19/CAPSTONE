879d5ad6704d7997fa25fcfcebba74a915bb6b7b
abena19 <ofosuabena19@gmail.com>
2022-07-21 11:59:54 -0700

    Add Google maps sdk to app framework

--- Dorm Room Wall/ViewControllers/WallFeedController.m
+++ Dorm Room Wall/ViewControllers/WallFeedController.m
@@ -14,6 +14,7 @@
 #import "WallCell.h"
 #import "WallHeaderView.h"
 #import "ParseQueryManager.h"
+@import GoogleMaps;
 
 
 @interface WallFeedController () <UITableViewDataSource, UITableViewDelegate, UIScrollViewDelegate>
